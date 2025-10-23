46b00b1e454ad2d462a8835d84a38029d96d42a8
Manideep Kotte <manideepkotte@gmail.com>
2025-10-23 09:46:59 +0530

    changes

--- config.py
+++ config.py
@@ -9,15 +9,15 @@
 NEO4J_URI = "neo4j+ssc://7b94e15a.databases.neo4j.io:7687"
 NEO4J_DATABASE = "neo4j"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "a7LtUEx2fxXPUIZTrOwebbpH5vhVTg6aGNQWzHvfAcM"
+NEO4J_PASSWORD = "a7Lt...."
 
 OPENAI_API_KEY = "sk-proj-......" # your OpenAI API key
-GOOGLE_API_KEY = "AIzaSyAaP0Qr9dPN6jrsFK1ToN1JS6GR-X6NYLI"  # optional: your Google Generative AI API key
+GOOGLE_API_KEY = "AIza...."  # optional: your Google Generative AI API key
 
 # Choose which chat provider to use: "openai" or "google"
 CHAT_PROVIDER = "google"
 
-PINECONE_API_KEY = "pcsk_2Mw7x5_Fyfzh8DGg9Gwa8Cs3yyuwz6GFyTEdJpywr7jnqYfMn8GiALTm4wJuEV7PbnD8CV" # your Pinecone API key
+PINECONE_API_KEY = "pcsk_....." # your Pinecone API key
 # Pinecone serverless config
 # Free plans commonly allow AWS regions like "us-east-1" or "us-west-2".
 PINECONE_CLOUD = "aws"
